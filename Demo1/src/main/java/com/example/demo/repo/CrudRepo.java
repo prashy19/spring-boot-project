a5aa92c9c44d511911e754a2d38a847c2cdad837
Prashant Mishra <prashant.mishra@sita.aero>
2023-07-11 08:43:23 +0530

    first commit

---
+++ Demo1/src/main/java/com/example/demo/repo/CrudRepo.java
@@ -0,0 +1,18 @@
+package com.example.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Product;
+
+public interface CrudRepo extends JpaRepository<Product, Integer>{
+
+
+//		find
+//		findall
+//    	findbyId
+//    	delete
+//    	deleteall
+//    	update
+//    	updateall
+
+}
